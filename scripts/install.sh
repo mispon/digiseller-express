87d66797f327c4cfc152681ffbaea8ac1dea22cd
bartem <bartem@ozon.ru>
2023-03-21 00:59:27 +0300

    get scripts via git

--- scripts/install.sh
+++ scripts/install.sh
@@ -3,15 +3,14 @@
 # Update system
 sudo apt update -y && sudo apt upgrade -y
 
+# Install git if not exist
+sudo apt install git
+
 # Create service folder
 mkdir digi-express && cd digi-express
 
 # Download service files
-curl -O "https://gist.githubusercontent.com/mispon/8613e6a133d2eab625c60ffcf70c9e9c/raw/4893d4b3dee8f6e668abe56867ddd466a73d5adf/docker-compose.yaml"
-curl -O "https://gist.githubusercontent.com/mispon/8613e6a133d2eab625c60ffcf70c9e9c/raw/4893d4b3dee8f6e668abe56867ddd466a73d5adf/create_tables.sql"
-curl -O "https://gist.githubusercontent.com/mispon/8613e6a133d2eab625c60ffcf70c9e9c/raw/4893d4b3dee8f6e668abe56867ddd466a73d5adf/run.sh"
-curl -O "https://gist.githubusercontent.com/mispon/8613e6a133d2eab625c60ffcf70c9e9c/raw/4893d4b3dee8f6e668abe56867ddd466a73d5adf/update.sh"
-curl -O "https://gist.githubusercontent.com/mispon/8613e6a133d2eab625c60ffcf70c9e9c/raw/4893d4b3dee8f6e668abe56867ddd466a73d5adf/repair.sh"
+git clone https://gist.github.com/8613e6a133d2eab625c60ffcf70c9e9c.git .
 
 # Grant access and run service
 chmod 777 repair.sh
